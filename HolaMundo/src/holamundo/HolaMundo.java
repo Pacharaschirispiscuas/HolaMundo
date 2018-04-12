fc7cfe65ea15ee86428b5a61c4cd44fbbc4c1bc4
Antonio Sánchez <antoniosanchez-03@hotmail.com>
2018-04-12 12:35:14 -0500

    Creacion del Hola Mundo

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -15,7 +15,7 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo"); // Cración del Hola Mundo 
         // TODO code application logic here
     }
     
